06fa32bf0fb41247c12271af49efe157855728b9
Abrar Mahir Esam <146331891+Orion-118@users.noreply.github.com>
2023-09-28 16:19:02 +0600

    Update demo code.py

--- demo code.py
+++ demo code.py
@@ -6,4 +6,7 @@ random_num = rand.random()
 #but if you want to then use randint
 random_range = rand.randint(1,10)
 
+#now lets do some calculations using this random number
+passowrd = str(random_range) + " hello i am random"
+
 print(random_num, random_range)
